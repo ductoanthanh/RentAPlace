53b55973cfdb5d396daac3d9d1e734750f0499e5
ductoanthanh <toanth@metropolia.fi>
2019-04-05 00:35:16 +0300

    Set up rental actions and reducers & cacher service

---
+++ src/actions/index.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+import { FETCH_RENTAL_BY_ID_SUCCESS,
+         FETCH_RENTAL_BY_ID_INIT,
+         FETCH_RENTALS_SUCCESS } from './types';
+
+const fetchRentalByIdInit = () => {
+  return {
+    type: FETCH_RENTAL_BY_ID_INIT
+  }
+}
+
+const fetchRentalByIdSuccess = (rental) => {
+  return {
+    type: FETCH_RENTAL_BY_ID_SUCCESS,
+    rental
+  }
+}
+
+const fetchRentalsSuccess = (rentals) => {
+  return {
+    type: FETCH_RENTALS_SUCCESS,
+    rentals
+  }
+}
+
+export const fetchRentals = () => {
+  return dispatch => {
+    axios.get('/api/v1/rentals')
+      .then(res => res.data )
+      .then(rentals => dispatch(fetchRentalsSuccess(rentals))
+    );
+  }
+}
+
+export const fetchRentalById = (rentalId) => {
+  return function(dispatch) {
+    dispatch(fetchRentalByIdInit());
+
+    axios.get(`/api/v1/rentals/${rentalId}`)
+      .then(res => res.data )
+      .then(rental => dispatch(fetchRentalByIdSuccess(rental))
+    );
+  }
+}
