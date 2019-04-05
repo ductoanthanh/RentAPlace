b51373d112381e2b50fc0862a2e8631b53729bbb
ductoanthanh <toanth@metropolia.fi>
2019-04-06 00:49:02 +0300

    Add RentalListing component

---
+++ src/components/rental/rental-listing/RentalListing.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { RentalList } from './RentalList';
+import { connect } from 'react-redux';
+import * as actions from 'actions/index.js';
+
+class RentalListing extends Component {
+   componentWillMount() {
+    this.props.dispatch(actions.fetchRentals());
+  }
+
+  render() {
+    return (
+      <section id="rentalListing">
+        <h1 className="page-title">Your Home All Around the World</h1>
+        <RentalList rentals={this.props.rentals} />
+      </section>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    rentals: state.rentals.data
+  }
+}
+
+export default connect(mapStateToProps)(RentalListing)
