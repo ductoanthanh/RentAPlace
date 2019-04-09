03b95489c7c0479105705af42e47265548f3396d
ductoanthanh <toanth@metropolia.fi>
2019-04-10 01:11:08 +0300

    Finalise user authentication & registration

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const User = require('../controllers/user');
 const router = express.Router();
+const User = require('../controllers/user');
 
 router.post('/auth', User.auth);
 
