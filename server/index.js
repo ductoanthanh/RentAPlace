c56f94ae90b771566845665c1340b88e19860752
ductoanthanh <arsenaltoan10@gmail.com>
2019-04-13 01:41:50 +0300

    Finalise rental routes - get & create & update & delete rental

--- server/index.js
+++ server/index.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const config = require('./config/dev');
+const config = require('./config');
 const FakeDb = require('./fake-db');
-const Rental = require('./models/rental')
+const path = require('path');
+
+const Rental = require('./models/rental');
 
 const rentalRoutes = require('./routes/rentals'),
-      userRoutes = require('./routes/users');
+      userRoutes = require('./routes/users'),
+      bookingRoutes = require('./routes/bookings');
 
 mongoose.connect(config.DB_URI).then(() => {
-  const fakeDb = new FakeDb();
-  fakeDb.seedDb();
+  if (process.env.NODE_ENV !== 'production') {
+    const fakeDb = new FakeDb();
+    // fakeDb.seedDb();
+  }
 });
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/bookings', bookingRoutes);
 
-const PORT = process.env.PORT || 3001;
+if (process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'build');
+  app.use(express.static(appPath));
+   app.get('*', function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 9999;
 
 app.listen(PORT , function() {
-  console.log('App is running!');
+  console.log(`App is running at ${PORT}!`);
 });
