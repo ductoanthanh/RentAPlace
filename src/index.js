444b800f34eb3a2197841d817a1538be7f39e3bc
ductoanthanh <arsenaltoan10@gmail.com>
2019-04-18 02:08:41 +0300

    Add all actions & reducers

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
- ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-daterangepicker/daterangepicker.css';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
