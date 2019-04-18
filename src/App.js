79896d1023c9edb35396f167f423bddd79ec2683
ductoanthanh <toanth@metropolia.fi>
2019-04-19 02:14:45 +0300

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
- class App extends Component {
+import 'App.css';
+import { ToastContainer } from 'react-toastify';
+import Header from 'components/shared/Header';
+import RentalListing from 'components/rental/rental-listing/RentalListing';
+import RentalDetail from 'components/rental/rental-detail/RentalDetail';
+import RentalSearchListing from 'components/rental/rental-listing/RentalSearchListing';
+import { RentalCreate } from 'components/rental/rental-create/RentalCreate';
+
+import Login from 'components/login/Login';
+import Register from 'components/register/Register';
+
+import { RentalManage } from 'components/rental/rental-manage/RentalManage';
+import BookingManage from 'components/booking/booking-manage/BookingManage';
+
+import { ProtectedRoute } from 'components/shared/auth/ProtectedRoute';
+import { LoggedInRoute } from 'components/shared/auth/LoggedInRoute';
+
+import * as actions from 'actions';
+
+const store = require('./reducers').init();
+
+class App extends Component {
+  componentWillMount() {
+    this.checkAuthState();
+  }
+
+  checkAuthState() {
+    store.dispatch(actions.checkAuthState());
+  }
+
+  logout() {
+    store.dispatch(actions.logout());
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className='App'>
+            <ToastContainer />
+            <Header logout={this.logout} />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' render={() =>  <Redirect to='/rentals' /> }/>
+                <Route exact path='/rentals' component={RentalListing} />
+                <Route exact path='/rentals/:city/homes' component={RentalSearchListing} />
+                <ProtectedRoute exact path='/rentals/manage' component={RentalManage} />
+                <ProtectedRoute exact path='/bookings/manage' component={BookingManage} />
+                <ProtectedRoute exact path='/rentals/new' component={RentalCreate} />
+                <Route exact path='/rentals/:id' component={RentalDetail} />
+                <Route exact path='/login' component={Login} />
+                <LoggedInRoute exact path='/register' component={Register} />
+              </Switch>
+            </div>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
 
- export default App;
+export default App;
